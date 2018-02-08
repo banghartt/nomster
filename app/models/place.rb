7bf2ed18c7ce4d16d1da7ec05207cf11b5eeac75
Thomas Banghart <banghartt@gmail.comgit config --global user.name Thoma>
2018-02-08 20:15:10 +0000

    Added comment db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,13 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, :description, :address, presence: true
   validates :name, length: { minimum: 4 }, presence: true
+
+
   
 end
