794a236d3b4f95a15556f9f6f855a1e2acda3c2a
Thomas Banghart <banghartt@gmail.comgit config --global user.name Thoma>
2018-02-24 08:00:30 +0000

    Attempting to fix map error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ before_action :authenticate_user!, only: [:create]
   def create
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
-      redirect_to place_path(@place), notice: "Comment successfully added" 
+    redirect_to place_path(@place), notice: "Comment successfully added" 
     end
 
 
