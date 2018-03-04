0c26792e847931d2b230c2e93b7ba910f2eac9ef
Thomas Banghart <banghartt@gmail.comgit config --global user.name Thoma>
2018-03-04 18:18:45 +0000

    Working on email functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,18 @@
 Rails.application.configure do
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
+
   config.action_mailer.default_url_options = { host: 'localhost:3030'}
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -23,7 +36,7 @@ Rails.application.configure do
       'Cache-Control' => 'public, max-age=172800'
     }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
 
     config.cache_store = :null_store
   end
